f15a7098b584d778fe5e0eeb5cc9925ef0f81443
JKearnsl <boyarshinovn@gmail.com>
2023-09-17 23:24:47 +0300

    Инициализировано представление

--- src/views/test/static_ui.py
+++ src/views/test/static_ui.py
@@ -29,16 +29,42 @@ class UiTest:
         test_layout.setContentsMargins(0, 0, 0, 0)
         test_layout.setSpacing(10)
 
+        # Header
+        th_layout = QtWidgets.QHBoxLayout()
+        th_layout.setContentsMargins(0, 0, 0, 0)
+        th_layout.setSpacing(0)
+        test_layout.addLayout(th_layout)
+
         test_header = widgets_factory.heading2(parent=test)
         test_header.setObjectName("test_header")
-        test_layout.addWidget(test_header)
+        th_layout.addWidget(test_header)
         self.test_header = test_header
 
-        # Input
+        th_layout.addStretch(1)
 
+        start_button = widgets_factory.button(parent=test)
+        start_button.setObjectName("start_button")
+        start_button.setMinimumWidth(100)
+        self.start_button = start_button
+        th_layout.addWidget(start_button)
 
+        # Table
+        table = widgets_factory.table(parent=test)
+        table.setObjectName("table")
+        table.setColumnCount(4)
+        table.setRowCount(20)
+
+        table.setHorizontalHeaderLabels([
+            "Тип", "Время работы", "Последовательность", "Случай"
+        ])
+
+        test_layout.addWidget(table)
+        self.table = table
+
+        central_layout.addLayout(test_layout)
         self.translate_ui(test)
         QtCore.QMetaObject.connectSlotsByName(test)
 
     def translate_ui(self, test: QtWidgets.QWidget):
         _translate = QtCore.QCoreApplication.translate
+        self.start_button.setText(_translate("test", "Начать"))
