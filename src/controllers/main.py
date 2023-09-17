8bd54d3a77a993822832c786d476919c14b12b67
JKearnsl <boyarshinovn@gmail.com>
2023-09-17 20:00:12 +0300

    Теперь страницы сортировок имеют единый контроллер

--- src/controllers/main.py
+++ src/controllers/main.py
@@ -1,23 +1,18 @@
-from src.controllers.exchange_sort import ExchangeSortController
-from src.controllers.fast_sort import FastSortController
-from src.controllers.heap_sort import HeapSortController
-from src.controllers.merge_sort import MergeSortController
 from src.controllers.settings import SettingsController
-from src.controllers.insertion_sort import InsertionSortController
-from src.controllers.selection_sort import SelectionSortController
-from src.controllers.shell_sort import ShellSortController
-from src.controllers.tree_sort import TreeSortController
-from src.models.exchange_sort import ExchangeSortModel
-from src.models.fast_sort import FastSortModel
-from src.models.heap_sort import HeapSortModel
-from src.models.merge_sort import MergeSortModel
+from src.controllers.sort import SortController
+
+from src.models.sort.exchange import ExchangeSortModel
+from src.models.sort.fast import FastSortModel
+from src.models.sort.heap import HeapSortModel
+from src.models.sort.merge import MergeSortModel
+from src.models.sort.selection import SelectionSortModel
+from src.models.sort.shell import ShellSortModel
+from src.models.sort.tree import TreeSortModel
 
 from src.models.settings import SettingsModel
-from src.models.insertion_sort import InsertionSortModel
+from src.models.sort.insertion import InsertionSortModel
 from src.models.main import MainModel
-from src.models.selection_sort import SelectionSortModel
-from src.models.shell_sort import ShellSortModel
-from src.models.tree_sort import TreeSortModel
+from src.utils.sort import SortType
 from src.views.main import MainView
 
 
@@ -36,42 +31,25 @@ class MainController:
             SettingsModel(self.model.config, self.model.theme), self.widgets_factory, self.view
         )
 
-    def load_insertion_sort(self):
-        controller = InsertionSortController(
-            InsertionSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
+    def show_sort(self, sort_id):
+        match sort_id:
+            case SortType.SELECT:
+                model = SelectionSortModel(self.model.config, self.model.theme)
+            case SortType.INSERTION:
+                model = InsertionSortModel(self.model.config, self.model.theme)
+            case SortType.SHELL:
+                model = ShellSortModel(self.model.config, self.model.theme)
+            case SortType.TREE:
+                model = TreeSortModel(self.model.config, self.model.theme)
+            case SortType.FAST:
+                model = FastSortModel(self.model.config, self.model.theme)
+            case SortType.HEAP:
+                model = HeapSortModel(self.model.config, self.model.theme)
+            case SortType.MERGE:
+                model = MergeSortModel(self.model.config, self.model.theme)
+            case SortType.EXCHANGE:
+                model = ExchangeSortModel(self.model.config, self.model.theme)
+            case _:
+                raise ValueError(f"Unknown sort type: {sort_id}")
 
-    def load_selection_sort(self):
-        controller = SelectionSortController(
-            SelectionSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
-
-    def load_exchange_sort(self):
-        controller = ExchangeSortController(
-            ExchangeSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
-
-    def load_fast_sort(self):
-        controller = FastSortController(
-            FastSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
-
-    def load_tree_sort(self):
-        controller = TreeSortController(
-            TreeSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
-
-    def load_heap_sort(self):
-        controller = HeapSortController(
-            HeapSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
-
-    def load_shell_sort(self):
-        controller = ShellSortController(
-            ShellSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
-
-    def load_merge_sort(self):
-        controller = MergeSortController(
-            MergeSortModel(self.model.config, self.model.theme), self.widgets_factory, self.view
-        )
+        controller = SortController(model, self.widgets_factory, self.view)
